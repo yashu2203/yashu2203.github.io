41bb2caab4311a54b7877d3b71192c4f6223e835
yashu2203 <yashu2203@gmail.com>
2017-01-06 21:53:01 +0530

    Code refactoring
    Addition of seperate css and js files

---
+++ js/app.js
@@ -0,0 +1,23 @@
+ function navigateTo(url){
+                
+                window.open(url,"_blank");
+            }
+            $(document).ready(function(){
+                
+                  $('#facebook-icon').on('click',function(){
+                navigateTo("http://www.facebook.com/yashu2203");
+            });
+            
+            $('#instagram-icon').on('click',function(){
+                navigateTo("http://www.instagram.com/rayo.22/");
+            });
+            
+            
+            $('#twitter-icon').click(function(){
+                navigateTo("http://twitter.com/Yashu_2203");
+            });
+                
+            });
+            
+        
+          
